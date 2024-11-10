3645a29e3778695e9bde7fbbcf976f7498afb84d
Gus Garcia <167840052+TokkitoZ@users.noreply.github.com>
2024-11-10 02:39:53 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Opa Leodete, já foi pra academia hoje? hmmm te amo</h1>
+    return <h1>Opa Leodete, já foi pra academia hoje? hmmm te amo ❤️</h1>
 }
 
 export default Home;
